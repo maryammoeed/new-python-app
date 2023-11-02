23ab13d033c3db960de4418c2f119c157efa9608
maryammoeed <maryammoeed68@gmail.com>
2023-11-03 01:42:42 +0500

    all files added

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python
+WORKDIR /app
+COPY requirements.txt /app/
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+COPY . /app/
+EXPOSE 5000
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Addition function
+def add(a, b):
+    return a + b
+
+# Subtraction function
+def subtract(a, b):
+    return a - b
+
+# Multiplication function
+def multiply(num1, num2):
+    return num1 * num2
+
+# Division function
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero"
+    return num1 / num2
+
+# Modulus function
+def mod(num1, num2):
+    return num1 % num2
+
+@app.route('/add/<int:num1>/<int:num2>', methods=['GET'])
+def addRoute(num1, num2):
+    return str(add(num1, num2))
+
+@app.route('/subtract/<int:num1>/<int:num2>')
+def subtractRoute(num1, num2):
+    return str(subtract(num1, num2))
+
+@app.route('/multiply/<int:num1>/<int:num2>')
+def multiplyRoute(num1, num2):
+    return str(multiply(num1, num2))
+
+@app.route('/divide/<int:num1>/<int:num2>')
+def divideRoute(num1, num2):
+    return str(divide(num1, num2))
+
+@app.route('/mod/<int:num1>/<int:num2>')
+def modRoute(num1, num2):
+    return str(mod(num1, num2))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
